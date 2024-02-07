034d740b4f6271b38b3c14b4019e7163e3564b7e
Mennatalla-Khougha <menmen.khougha@gmail.com>
2024-02-07 17:11:36 +0200

    task 0

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,6 +2,23 @@
 """Filter file"""
 from typing import List
 import re
+import logging
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+    """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        NotImplementedError
 
 
 def filter_datum(
@@ -25,6 +42,15 @@ def filter_datum(
     str: The log message obfuscated
     """
 
-    regex = '|'.join(fields)
-    print(f'({regex})=[^{separator}]*')
-    return re.sub(f'({regex})=[^{separator}]*', f'\\1={redaction}', message)
+    # regex = '|'.join(fields)
+    # print(f'({regex})=[^{separator}]*')
+    # return re.sub(
+    #     f'{regex}=.*?{separator}]',
+    #     f'\\1={redaction}',
+    #     message)
+
+
+    for field in fields:
+        pattern = r'{}=[^{}]+'.format(field, separator)
+        message = re.sub(pattern, '{}={}'.format(field, redaction), message)
+    return message
