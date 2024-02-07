956399377210a64f5bebf1f6f1a6b0039c8ffaa0
Mennatalla-Khougha <menmen.khougha@gmail.com>
2024-02-07 17:29:31 +0200

    task 0

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -17,7 +17,7 @@ class RedactingFormatter(logging.Formatter):
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
-    def format(self, record: logging.LogRecord) -> str:
+    def format(self, record: logging.LogRecord):
         NotImplementedError
 
 
@@ -41,12 +41,12 @@ def filter_datum(
     return:
     str: The log message obfuscated
     """
-    # for field in fields:
-    #     message = re.sub(
-    #         r'{}=[^{}]+'.format(field, separator),
-    #         '{}={}'.format(field, redaction),
-    #         message)
-    # return message
-    return re.sub(r'({})=[^{}]+'.format(
-        '|'.join(fields), separator
-        ), r'\1={}'.format(redaction), message)
+    for field in fields:
+        message = re.sub(
+            r'{}=[^{}]+'.format(field, separator),
+            '{}={}'.format(field, redaction),
+            message)
+    return message
+    # return re.sub(r'({})=[^{}]+'.format(
+    #     '|'.join(fields), separator
+    #     ), r'\1={}'.format(redaction), message)
