639bda3d5fb4c7726c25e166b57ad7e4231d8d5f
Mennatalla-Khougha <menmen.khougha@gmail.com>
2024-02-07 18:23:58 +0200

    task 3

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,6 +3,8 @@
 from typing import List
 import re
 import logging
+import os
+import mysql.connector
 
 
 def filter_datum(
@@ -87,3 +89,20 @@ def get_logger() -> logging.Logger:
     logger.addHandler(stream_handler)
 
     return logger
+
+
+def get_db():
+    """Returns a connector to the database"""
+    db_username = os.getenv("PERSONAL_DATA_DB_USERNAME", "root")
+    db_password = os.getenv("PERSONAL_DATA_DB_PASSWORD", "")
+    db_host = os.getenv("PERSONAL_DATA_DB_HOST", "localhost")
+    db_name = os.getenv("PERSONAL_DATA_DB_NAME")
+
+    connection = mysql.connector.connect(
+        host=db_host,
+        user=db_username,
+        password=db_password,
+        database=db_name
+    )
+
+    return connection
