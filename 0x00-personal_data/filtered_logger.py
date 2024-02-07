ce37e4ae2fd2b4cc17ed621ff8727196570ea75b
Mennatalla-Khougha <menmen.khougha@gmail.com>
2024-02-07 18:27:58 +0200

    task 3

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -91,7 +91,7 @@ def get_logger() -> logging.Logger:
     return logger
 
 
-def get_db():
+def get_db() -> mysql.connector.connection.MySQLConnection:
     """Returns a connector to the database"""
     username = os.getenv("PERSONAL_DATA_DB_USERNAME", "root")
     password = os.getenv("PERSONAL_DATA_DB_PASSWORD", "")
