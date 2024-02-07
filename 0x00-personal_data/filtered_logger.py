f98ca4d78d01d76f915e7a69b27ac6a9499915ca
Mennatalla-Khougha <menmen.khougha@gmail.com>
2024-02-07 17:47:24 +0200

    task 1

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -5,32 +5,6 @@ import re
 import logging
 
 
-class RedactingFormatter(logging.Formatter):
-    """ Redacting Formatter class
-    """
-
-    REDACTION = "***"
-    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
-    SEPARATOR = ";"
-
-    def __init__(self, fields: List[str]):
-        """_summary_
-
-        Args:
-            fields (List[str]): _description_
-        """
-        super(RedactingFormatter, self).__init__(self.FORMAT)
-        self.fields = fields
-
-    def format(self, record: logging.LogRecord):
-        """_summary_
-
-        Args:
-            record (logging.LogRecord): _description_
-        """
-        NotImplementedError
-
-
 def filter_datum(
         fields: List[str],
         redaction: str,
@@ -57,3 +31,33 @@ def filter_datum(
             '{}={}'.format(field, redaction),
             message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+    """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """Initialize the class
+
+        Args:
+            fields (List[str]):List of strings representing all fields
+                            to obfuscate
+        """
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """ filter values in incoming log records using filter_datum
+
+        Args:
+            record (logging.LogRecord): values in incoming log records
+        """
+        record.msg = filter_datum(self.fields,
+                                  self.REDACTION, record.msg,
+                                  self.SEPARATOR)
+        return super().format(record)
