695c16d39fd8745031cab8d31f31242b2f322466
Mennatalla-Khougha <menmen.khougha@gmail.com>
2024-02-08 13:27:17 +0200

    task 3

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -5,8 +5,7 @@ import re
 import logging
 import os
 from os import environ
-import mysql.connector
-from mysql.connector import (connection)
+from mysql.connector import connection
 
 
 def filter_datum(
@@ -93,47 +92,13 @@ def get_logger() -> logging.Logger:
     return logger
 
 
+def get_db() -> connection.MySQLConnection:
+    """Returns a connector to the database"""
+    username = os.getenv("PERSONAL_DATA_DB_USERNAME", "root")
+    password = os.getenv("PERSONAL_DATA_DB_PASSWORD", "")
+    host = os.getenv("PERSONAL_DATA_DB_HOST", "localhost")
+    database = os.getenv("PERSONAL_DATA_DB_NAME")
 
-def get_db() -> mysql.connector.connection.MySQLConnection:
-    """
-    Returns a MySQLConnection object for accessing Personal Data database
-
-    Returns:
-        A MySQLConnection object using connection details from
-        environment variables
-    """
-    username = environ.get("PERSONAL_DATA_DB_USERNAME", "root")
-    password = environ.get("PERSONAL_DATA_DB_PASSWORD", "")
-    host = environ.get("PERSONAL_DATA_DB_HOST", "localhost")
-    db_name = environ.get("PERSONAL_DATA_DB_NAME")
-
-    cnx = mysql.connector.connection.MySQLConnection(user=username,
-                                                     password=password,
-                                                     host=host,
-                                                     database=db_name)
-    return cnx
-
-
-def main():
-    """
-    Main function to retrieve user data from database and log to console
-    """
-    db = get_db()
-    cursor = db.cursor()
-    cursor.execute("SELECT * FROM users;")
-    field_names = [i[0] for i in cursor.description]
-
-    logger = get_logger()
-
-    for row in cursor:
-        str_row = ''.join(f'{f}={str(r)}; ' for r, f in zip(row, field_names))
-        logger.info(str_row.strip())
-
-    cursor.close()
-    db.close()
-
-
-if __name__ == '__main__':
-    main()
-
-
+    return connection.MySQLConnection(
+        user=username, password=password,
+        host=host, database=database)
