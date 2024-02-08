a759ebb79e7df81c807e8e589efdfa97027a2e5d
Mennatalla-Khougha <menmen.khougha@gmail.com>
2024-02-08 13:54:07 +0200

    task 6

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -16,3 +16,16 @@ def hash_password(password: str) -> bytes:
     salt = bcrypt.gensalt()
     hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
     return hashed_password
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """Checks if a given password matches a previously hashed password
+
+    Args:
+        hashed_password (bytes): hashed password to be checked
+        password (str): password
+
+    Returns:
+        bool: True or False
+    """
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
