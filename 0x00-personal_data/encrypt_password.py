7bbcdaca55fff491fc2a649293055dca5668c4a1
Mennatalla-Khougha <menmen.khougha@gmail.com>
2024-02-08 13:47:40 +0200

    task 5

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,1 +1,18 @@
 #!/usr/bin/env python3
+import bcrypt
+"""Hashing password"""
+
+
+def hash_password(password: str) -> bytes:
+    """hash_password function that expects one string argument name password
+    and returns a salted, hashed password, which is a byte string
+
+    Args:
+        password (str): password to be hashed
+
+    Returns:
+        bytes: hashed password
+    """
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+    return hashed_password
--- 0x00-personal_data/main.py
+++ 0x00-personal_data/main.py
@@ -2,13 +2,9 @@
 """
 Main file
 """
-from filtered_logger import get_db
-# get_db = __import__('filtered_logger').get_db
 
-db = get_db()
-cursor = db.cursor()
-cursor.execute("SELECT COUNT(*) FROM users;")
-for row in cursor:
-    print(row[0])
-cursor.close()
-db.close()
+hash_password = __import__('encrypt_password').hash_password
+
+password = "MyAmazingPassw0rd"
+print(hash_password(password))
+print(hash_password(password))
