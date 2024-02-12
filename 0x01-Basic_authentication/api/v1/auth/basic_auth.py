d12a7caf6e721a1a74ec956972fe8e24ef99092b
Mennatalla-Khougha <menmen.khougha@gmail.com>
2024-02-12 21:14:03 +0200

    task 10

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -82,10 +82,13 @@ class BasicAuth(Auth):
             return None
         if user_pwd is None or not isinstance(user_pwd, str):
             return None
-        users = User.search({'email': user_email})
-        if not users or not len(users):
+        try:
+            users = User.search({'email': user_email})
+            if not users or not len(users):
+                return None
+            for user in users:
+                if user.is_valid_password(user_pwd):
+                    return user
             return None
-        for user in users:
-            if user.is_valid_password(user_pwd):
-                return user
-        return None
+        except Exception:
+            return None
