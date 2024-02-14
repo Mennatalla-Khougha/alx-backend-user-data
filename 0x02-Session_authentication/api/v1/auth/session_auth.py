73c9d659d2aee8c7562cab3a69c78a0996789897
Mennatalla-Khougha <menmen.khougha@gmail.com>
2024-02-14 17:30:39 +0200

    task 2

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,7 +1,24 @@
 #!/usr/bin/env python3
 """class to manage the Session authentication"""
 from api.v1.auth.auth import Auth
+from uuid import uuid4
 
 
 class SessionAuth(Auth):
     """class to manage the Session authentication"""
+    user_id_by_session_id: dict = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """Creates a Session ID for a user_id
+
+        Args:
+            user_id (str): The User ID. Defaults to None.
+
+        Returns:
+            str: The session ID
+        """
+        if user_id is None or not isinstance(user_id, str):
+            return None
+        session_id = str(uuid4())
+        self.user_id_by_session_id[user_id] = session_id
+        return session_id
