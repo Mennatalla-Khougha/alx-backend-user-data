29871aa488a6cd1c9e6a9690efe10fbd4735e433
Mennatalla-Khougha <menmen.khougha@gmail.com>
2024-02-15 01:05:26 +0200

    task 8

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -35,7 +35,7 @@ def session_login():
     return response
 
 
-@app_views.route('/api/v1/auth_session/logout',
+@app_views.route('/auth_session/logout',
                  methods=['DELETE'], strict_slashes=False)
 def session_logout():
     """DELETE /api/v1/auth_session/logout
