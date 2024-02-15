6cb5ffa77dc1407f02f6a855dcd09a9e27382dc4
Mennatalla-Khougha <menmen.khougha@gmail.com>
2024-02-15 20:19:34 +0200

    task 10

--- 0x02-Session_authentication/api/v1/auth/session_db_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_db_auth.py
@@ -1,59 +1,41 @@
 #!/usr/bin/env python3
-"""
-Define class SessionDButh
-"""
-from .session_exp_auth import SessionExpAuth
+"""User session model"""
+from api.v1.auth.session_exp_auth import SessionExpAuth
 from models.user_session import UserSession
 
 
 class SessionDBAuth(SessionExpAuth):
-    """
-    Definition of SessionDBAuth class that persists session data
-    in a database
-    """
-
+    """Authentication class"""
     def create_session(self, user_id=None):
-        """
-        Create a Session ID for a user_id
-        Args:
-           user_id (str): user id
-        """
+        """Overload the create session method"""
         session_id = super().create_session(user_id)
         if not session_id:
             return None
-        kw = {
-            "user_id": user_id,
-            "session_id": session_id
-        }
-        user = UserSession(**kw)
-        user.save()
+        arg = {"user_id": user_id, "session_id": session_id}
+        user_session = UserSession(**arg)
+        user_session.save()
+        user_session.save_to_file()
         return session_id
 
     def user_id_for_session_id(self, session_id=None):
-        """
-        Returns a user ID based on a session ID
-        Args:
-            session_id (str): session ID
-        Return:
-            user id or None if session_id is None or not a string
-        """
-        user_id = UserSession.search({"session_id": session_id})
-        if user_id:
-            return user_id
-        return None
+        """Overload user id for session id"""
+        if not session_id:
+            return None
+        UserSession.load_from_file()
+        user_session = UserSession.search({"session_id": session_id})
+        if not user_session:
+            return None
+        return user_session[0].user_id
 
     def destroy_session(self, request=None):
-        """
-        Destroy a UserSession instance based on a
-        Session ID from a request cookie
-        """
-        if request is None:
+        """Overload destroy session"""
+        if not request:
             return False
         session_id = self.session_cookie(request)
         if not session_id:
             return False
         user_session = UserSession.search({"session_id": session_id})
-        if user_session:
-            user_session[0].remove()
-            return True
-        return False
+        if not user_session:
+            return False
+        del user_session[0]
+        return True
