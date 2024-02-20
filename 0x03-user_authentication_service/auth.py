78752b2c27fd5475fe6d8b1dc6c26b002ae96a5d
Mennatalla-Khougha <menmen.khougha@gmail.com>
2024-02-20 13:36:45 +0200

    task 4

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,1 +1,14 @@
 #!/usr/bin/env python3
+import bcrypt
+
+
+def _hash_password(password: str) -> bytes:
+    """Hash a password with bcrypt.
+    Args:
+        password (str): The password to be hashed.
+    Returns:
+        bytes: The salted hash of the password.
+    """
+    salt = bcrypt.gensalt()
+    hash_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+    return hash_password
