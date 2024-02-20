40be7e34996ba851098cf2aa9612e20be961796e
Mennatalla-Khougha <menmen.khougha@gmail.com>
2024-02-20 12:27:50 +0200

    task 1

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -6,7 +6,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
 
-from user import Base
+from user import Base, User
 
 
 class DB:
@@ -29,3 +29,19 @@ class DB:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
         return self.__session
+
+    @staticmethod
+    def add_user(self, email: str, hashed_password: str) -> User:
+        """ The method should save the user to the database
+
+        Args:
+            email (str): User's Email
+            hashed_password (str): User's hashed password
+
+        Returns:
+            User (object): User object
+        """
+        user = User(email=email, hashed_password=hashed_password)
+        self._session.add(user)
+        self._session.commit(user)
+        return user
