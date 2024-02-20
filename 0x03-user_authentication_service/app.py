4edd70af5cdd9ce3a1f34ca2a472c11ba05896f2
Mennatalla-Khougha <menmen.khougha@gmail.com>
2024-02-20 19:10:56 +0200

    testing for a friend

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,1 +1,12 @@
 #!/usr/bin/env python3
+from flask import Flask, jsonify
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def base_route():
+    return jsonify({"message": "Bienvenue"})
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
