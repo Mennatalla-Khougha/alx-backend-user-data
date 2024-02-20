23277efaa4fb40a1d4d3b29d38c3bf643b441282
Mennatalla-Khougha <menmen.khougha@gmail.com>
2024-02-20 19:11:36 +0200

    testing for a friend

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -40,7 +40,7 @@ class DB:
     def find_user_by(self, **kwargs: Dict[str, str]) -> User:
         """Find a user in the database based on the specified criteria."""
         if not kwargs:
-            raise InvalidRequestError
+            raise InvalidRequestError()
         try:
             user = self._session.query(User).filter_by(**kwargs).one()
         except NoResultFound:
