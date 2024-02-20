bc310168a806ad3645b869b9e2b7807a7fe7f954
Mennatalla-Khougha <menmen.khougha@gmail.com>
2024-02-20 12:57:11 +0200

    task 1

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,6 +5,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.exc import NoResultFound, InvalidRequestError
 
 from user import Base, User
 
@@ -44,3 +45,23 @@ class DB:
         self._session.add(user)
         self._session.commit()
         return user
+
+    def find_user_by(self, **kwargs) -> User:
+        """Find a user by arbitrary keyword arguments.
+
+        Args:
+            **kwargs: Keyword arguments to filter the User table.
+
+        Returns:
+            User: The first User object found that matches the filter.
+
+        Raises:
+            NoResultFound: If no user is found that matches the filter.
+            InvalidRequestError: If the query arguments are invalid.
+        """
+        try:
+            return self._session.query(User).filter_by(**kwargs).first()
+        except NoResultFound:
+            raise NoResultFound("Not found")
+        except InvalidRequestError:
+            raise InvalidRequestError
