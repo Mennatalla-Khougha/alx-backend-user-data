6c7859f116cdc97b8c4ceed12eddb913b84286f5
Mennatalla-Khougha <menmen.khougha@gmail.com>
2024-02-21 13:26:18 +0200

    task 20

--- 0x03-user_authentication_service/main.py
+++ 0x03-user_authentication_service/main.py
@@ -37,22 +37,36 @@ def profile_unlogged() -> None:
 
 def log_in(email: str, password: str) -> str:
     """Logs in with the specified email and password, returning session ID"""
-    return None
+    res = requests.post(BASE_URL + "/sessions",
+                        data={'email': email, "password": password})
+    assert(res.status_code == 200)
+    assert(res.json() == {"email": email, "message": "logged in"})
+    return res.cookies['session_id']
 
 
 def profile_logged(session_id: str) -> None:
     """Accesses the profile endpoint with a valid session ID."""
-    pass
+    cookies = {'session_id': session_id}
+    res = requests.get(BASE_URL + '/profile', cookies=cookies)
+    assert(res.status_code == 200)
 
 
 def log_out(session_id: str) -> None:
     """Logs out the user with the specified session ID."""
-    pass
+    cookies = {'session_id': session_id}
+    res = requests.delete(BASE_URL + '/sessions', cookies=cookies)
+    if res.status_code == 302:
+        assert(res.url == BASE_URL)
+    else:
+        assert(res.status_code == 200)
 
 
 def reset_password_token(email: str) -> str:
     """Requests a password reset token for the specified email."""
-    return None
+    res = requests.post(BASE_URL + '/reset_password', data={'email': email})
+    if res.status_code == 200:
+        return res.json()['reset_token']
+    assert(res.status_code == 401)
 
 
 def update_password(email: str, reset_token: str, new_password: str) -> None:
@@ -60,7 +74,13 @@ def update_password(email: str, reset_token: str, new_password: str) -> None:
     Updates the password for the specified email using the provided
     reset token and new password.
     """
-    pass
+    data = {'email': email, 'reset_token': reset_token,
+            'new_password': new_password}
+    res = requests.put(BASE_URL + '/reset_password', data=data)
+    if res.status_code == 200:
+        assert(res.json() == {"email": email, "message": "Password updated"})
+    else:
+        assert(res.status_code == 403)
 
 
 if __name__ == "__main__":
