825322430b58e8e2dabe57cf6efa515660750309
Mennatalla-Khougha <menmen.khougha@gmail.com>
2024-02-21 07:28:31 +0200

    task 18

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -147,9 +147,9 @@ class Auth:
             # user.reset_token = None
         except NoResultFound:
             raise ValueError
-            hashed_password = _hash_password(password)
-            self._db.update_user(
-                user.id,
-                hashed_password=hashed_password,
-                reset_token=None
-                )
+        hashed_password = _hash_password(password)
+        self._db.update_user(
+            user.id,
+            hashed_password=hashed_password,
+            reset_token=None
+            )
